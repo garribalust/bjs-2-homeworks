179ee7f3d6b4848bb5faf32720232a8a2cc9bbfd
garribalust <garribalust@gmail.com>
2022-06-09 17:40:39 +0400

    Первое дз выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+   let discriminante = b ** 2 - 4 * a * c;
+
+   if (discriminante < 0) {
+      return []
+  } 
+   
+if (discriminante === 0) {
+     let newResult = - b / (2 * a);
+     return [newResult]
+   } 
+   
+if (discriminante > 0) {
+   let newPozitiv = (-b + Math.sqrt(discriminante) ) / (2 * a);
+   let newPozitiv2 = (-b - Math.sqrt(discriminante) ) / (2 * a);
+   return [newPozitiv, newPozitiv2]
+}   
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
