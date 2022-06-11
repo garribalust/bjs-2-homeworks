53421eb86bb37d41bb55a21d8ceed0c051d73e11
garribalust <garribalust@gmail.com>
2022-06-11 23:30:36 +0400

    исправлена задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,21 +2,18 @@
 
 function solveEquation(a, b, c) {
    let discriminante = b ** 2 - 4 * a * c;
-
-   if (discriminante < 0) {
-      return []
-  } 
+   let array = [];
    
-if (discriminante === 0) {
+   if (discriminante === 0) {
      let newResult = - b / (2 * a);
      return [newResult]
    } 
    
-if (discriminante > 0) {
+   if (discriminante > 0) {
    let newPozitiv = (-b + Math.sqrt(discriminante) ) / (2 * a);
    let newPozitiv2 = (-b - Math.sqrt(discriminante) ) / (2 * a);
    return [newPozitiv, newPozitiv2]
-}   
+}  return array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
