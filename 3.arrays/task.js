3af4c023c553a10e8df597db09d28aaa9dadd63e
garribalust <garribalust@gmail.com>
2022-06-20 12:07:30 +0400

    доработанная 1-я функция

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result = arr1.every(i => arr2.includes(i));
-      
-  return result;
-}
+   let result = arr1.every((elem, ind) => arr2.includes(elem, ind));
+       if (arr1.length < arr2.length) {
+         return false;
+       }
+   return result;
+ }
 
 function advancedFilter(arr) {
   let resultArr = arr.filter(item => item > 0).filter(i => i % 3 === 0).map(elem => elem * 10);
