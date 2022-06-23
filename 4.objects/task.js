9456e94dc1563862aaa3c8bfaacf33dcb89014a4
garribalust <garribalust@gmail.com>
2022-06-23 22:57:37 +0400

    испарвлена addMarks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -8,8 +8,6 @@ let student1 = new Student('Alex', 'male', 34);
 let student2 = new Student('Olga', 'female', 23);
 let student3 = new Student('Fedor', 'male', 29);
 
-
-
 Student.prototype.setSubject = function (subjectName) {
   this.subject = subjectName;
 }
@@ -24,14 +22,13 @@ Student.prototype.addMark = function (mark) {
 
  Student.prototype.addMarks = function (...mark1) {
    if(this.marks === undefined) {
-      this.marks = [mark1];
+      this.marks = mark1;
    } else {
       this.marks.push(...mark1);
    }
  }
 
  Student.prototype.getAverage = function () {
-   
    return this.marks.reduce((acc, item, index, marks) => {
       acc += item;
       if(index === marks.length - 1) {
